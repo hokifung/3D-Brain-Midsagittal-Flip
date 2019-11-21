e83185f788fc5b8b7ae2a078fa4045981904e296
hokifung <45206004+hokifung@users.noreply.github.com>
2019-11-22 02:02:32 +0800

    Code Refactoring + TQDM Progress Bar

---
+++ flip.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import copy
+from tqdm import tqdm
+import nibabel as nib
+from nibabel.testing import data_path
+
+
+##
+# Mirror Voxel Data by Defined Axis
+##
+def MirrorVoxels(axis_value, original_data):
+    for slice_number in range(0, int(original_data.shape[0] / 2)):
+            # Copy Data from Memory (Incase Numpy Uses Shallow Copy)
+            temporary_slice = copy.deepcopy(original_data[slice_number, :, :])
+            # Calculate Mirror Index
+            mirror_idx = original_data.shape[0] - 1 - slice_number
+            # Offset Additional 1 from Indexing
+            original_data[slice_number,:,:] = copy.deepcopy(original_data[mirror_idx,:,: ])
+            # Return Temporary Slice Data to Right Side
+            original_data[mirror_idx,:,:] = copy.deepcopy(temporary_slice)
+
+
+if __name__ == "__main__":
+    input_path  = sys.argv[1]
+    output_path = sys.argv[2]
+
+    print("Retrieving Files from {}".format(input_path))
+
+    # Open OS Directory
+    files = os.listdir(path=input_path)
+
+    # Iterate Through Filelist
+    for nfile in tqdm(range(len(files))):
+        ## Sanity Check
+        if ".nii" not in files[nfile]:
+            continue
+
+        output_name = output_path + "/flipped_" + files[nfile]
+        filepath = input_path + "/" + files[nfile]
+        main = nib.load(filepath)
+        header = main.header
+        main_data = main.get_fdata() 
+        edited_data = copy.deepcopy(main_data)
+        MirrorVoxels(26, edited_data) 
+        mirror = nib.Nifti1Image(edited_data, main.affine, main.header)
+        nib.save(mirror, output_name)
+ 
+    # Announce
+    print("Completed : Files Exported to /{}".format(output_path))
